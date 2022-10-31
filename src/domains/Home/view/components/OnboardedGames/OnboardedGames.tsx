0639f0f466489e591e5a75c5333b97f9b31665aa
schatzen <gomieti@naver.com>
2022-10-31 19:15:44 +0900

    update: 반응형시 grid 조정

--- src/domains/Home/view/components/OnboardedGames/OnboardedGames.tsx
+++ src/domains/Home/view/components/OnboardedGames/OnboardedGames.tsx
@@ -1,13 +1,15 @@
+import { useCustomMediaQuery } from "@/common/themes/UseCustomMediaQuery";
 import { TitleSection } from "@/domains/Home/common/components/TitleSection";
 import { css } from "@emotion/react";
 import { GameCard } from "./elements/GameCard";
 import { CardItems } from "./models/card.models";
 
 export const OnboardedGames = () => {
+  const { isMedium } = useCustomMediaQuery();
   return (
-    <div css={st.root}>
+    <div css={st.root(isMedium)}>
       <TitleSection title="Onboarded Games" onClick={() => alert("click")} />
-      <div css={st.container}>
+      <div css={st.container(isMedium)}>
         {CardItems.map((it, index) => (
           <GameCard key={index} src={it.src} title={it.title} desc={it.desc} />
         ))}
@@ -17,16 +19,17 @@ export const OnboardedGames = () => {
 };
 
 const st = {
-  root: css`
+  root: (isMedium: boolean) => css`
     width: 100%;
     max-width: 1200px;
+    padding: ${isMedium ? "0 16px" : "0 24px"};
     margin: 0 auto;
     margin-bottom: 240px;
   `,
-  container: css`
+  container: (isMedium: boolean) => css`
     width: 100%;
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-columns: ${isMedium ? "1fr 1fr" : "1fr 1fr 1fr"};
     column-gap: 3%;
     row-gap: 3%;
   `,
