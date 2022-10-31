e641af9d6d21372b7b8e9b94731a6c02f3b00de4
schatzen <gomieti@naver.com>
2022-10-31 16:50:52 +0900

    update: section 추가

--- src/domains/Home/view/HomeView/HomeView.tsx
+++ src/domains/Home/view/HomeView/HomeView.tsx
@@ -1,3 +1,21 @@
+import { Footer } from "@/common/components/Footer";
+import { Header } from "@/common/components/Header";
+import { HomeSwiper } from "../components/HomeSwiper";
+import { LatestNews } from "../components/LatestNews";
+import { OnboardedGames } from "../components/OnboardedGames";
+import { UpcomingGames } from "../components/UpcomingGames";
+
 export const HomeView = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <main>
+        <HomeSwiper />
+        <OnboardedGames />
+        <UpcomingGames />
+        <LatestNews />
+        <Footer />
+      </main>
+    </div>
+  );
 };
