f9dfe94aca25265d0b2b498bf9ae8d8f6efad6fa
schatzen <gomieti@naver.com>
2022-11-02 00:22:41 +0900

    feat: 양 옆 슬라이드 effect 적용 및 버튼 추가

--- src/domains/Home/view/components/HomeSwiper/swiper/SwiperSection.tsx
+++ src/domains/Home/view/components/HomeSwiper/swiper/SwiperSection.tsx
@@ -1,11 +1,16 @@
 import { css } from "@emotion/react";
-import SwiperCore, { Navigation, Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+import SwiperCore from "swiper";
 import SwiperItem1 from "@/assets/home/swiper/swiper-item-1.svg";
+import { Autoplay, EffectCoverflow } from "swiper";
 import Image from "next/image";
+import { useState } from "react";
+import { Button } from "@mui/material";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { useCustomMediaQuery } from "@/common/themes/UseCustomMediaQuery";
+import { MediaQueries } from "@/common/themes/Liimt";
 
 export const SwiperSection = () => {
   const SwiperItems = [
@@ -16,60 +21,105 @@ export const SwiperSection = () => {
     SwiperItem1,
     SwiperItem1,
   ];
+
+  const [swiper, setSwiper] = useState<SwiperCore>();
+  const slideNext = () => swiper?.slideNext();
+  const slidePrev = () => swiper?.slidePrev();
+
+  const { isMedium } = useCustomMediaQuery();
   return (
-    <div css={st.container}>
+    <div css={st.root}>
       <Swiper
-        slidesPerView={3}
-        spaceBetween={20}
+        spaceBetween={50}
+        slidesPerView={1.5}
+        initialSlide={1}
         centeredSlides={true}
         loop={true}
-        modules={[Pagination]}
-        pagination={{
-          clickable: true,
+        onSlideChange={(e) => console.log(e)}
+        effect={"coverflow"}
+        coverflowEffect={{
+          rotate: 0,
+          slideShadows: false,
+          scale: 0.8,
         }}
+        modules={[EffectCoverflow, Autoplay]}
+        onSwiper={(swiper) => setSwiper(swiper)}
       >
         {SwiperItems.map((it, index) => (
           <SwiperSlide key={index}>
-            <div css={st.slideImage}>
+            <div css={st.image}>
               <Image fill src={it} alt="slide" />
+              <div className="swiper-image-background"></div>
             </div>
           </SwiperSlide>
         ))}
-        {/*  
-        <SwiperSlide>
-          <div css={st.slideImage}>
-            <Image fill src={SwiperItem1} alt="slide" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div css={st.slideImage}>
-            <Image fill src={SwiperItem1} alt="slide" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div css={st.slideImage}>
-            <Image fill src={SwiperItem1} alt="slide" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div css={st.slideImage}>
-            <Image fill src={SwiperItem1} alt="slide" />
-          </div>
-        </SwiperSlide> */}
       </Swiper>
+      <div css={st.btnContainer}>
+        <Button onClick={slidePrev} color="secondary" css={st.btnWrapper}>
+          <div css={st.btn}>
+            <ChevronLeftIcon />
+          </div>
+        </Button>
+        <Button onClick={slideNext} color="secondary" css={st.btnWrapper}>
+          <div css={st.btn}>
+            <ChevronRightIcon />
+          </div>
+        </Button>
+      </div>
     </div>
   );
 };
 
 const st = {
-  container: css`
+  root: css`
     width: 100%;
-    padding: 96px 0;
+    position: relative;
   `,
 
-  slideImage: css`
+  image: css`
     position: relative;
-    width: 1200px;
+    width: 100%;
     aspect-ratio: 1/0.416;
+  `,
+
+  btnContainer: css`
+    width: 74.5%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 2;
+    display: flex;
+    justify-content: space-between;
+
+    @media ${MediaQueries.md} {
+      width: 77.7%;
+    }
+
+    @media ${MediaQueries.sm} {
+      width: 80%;
+    }
+
+    @media ${MediaQueries.xs} {
+      width: 86%;
+    }
+  `,
+  btnWrapper: css`
+    width: 3%;
+    aspect-ratio: 1;
+    min-width: 24px;
+    min-height: 24px;
+    aspect-ratio: 1;
+    padding: 0;
+  `,
+  btn: css`
+    width: 100%;
+    height: 100%;
+    background-color: black;
+    border-radius: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 0px 0px 10px #000000;
   `,
 };
