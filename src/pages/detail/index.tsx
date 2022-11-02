9367794d576d2fcb7c200edc1d702d17985a841f
schatzen <56854068+schatzen@users.noreply.github.com>
2022-11-02 19:04:07 +0900

    Create index.tsx

---
+++ src/pages/detail/index.tsx
@@ -0,0 +1,13 @@
+import { DetailView } from "@/domains/Detail/view";
+import type { NextPage } from "next";
+import React from "react";
+
+const DetailPage: NextPage = () => {
+  return (
+    <React.Fragment>
+      <DetailView />
+    </React.Fragment>
+  );
+};
+
+export default DetailPage;
