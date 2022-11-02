31302bbe548a6dbd72332892e84b7358a81b1095
schatzen <56854068+schatzen@users.noreply.github.com>
2022-11-02 19:02:07 +0900

    feat: banner 섹션 구현

---
+++ src/domains/Detail/view/components/Banner/Banner.tsx
@@ -0,0 +1,70 @@
+import { css } from "@emotion/react";
+import { Typography } from "@mui/material";
+import Image from "next/image";
+import BannerImg from "@/assets/detail/banner/img-banner.svg";
+
+export const Banner = () => {
+  const BannerItem = {
+    src: BannerImg,
+    caption: "Onboarded Games",
+    title: '"Become a King and save the kingdom from an obscure conspiracy"',
+  };
+
+  return (
+    <div css={st.root}>
+      <div css={st.image}>
+        <Image fill src={BannerItem.src} alt="img" />
+        <div css={st.opacity}></div>
+        <div css={st.textContainer}>
+          <Typography variant="caption" color="secondary" mb={0.4}>
+            {BannerItem.caption}
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={"40px"}
+            fontWeight={700}
+            lineHeight={1.4}
+          >
+            {BannerItem.title}
+          </Typography>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const st = {
+  root: css`
+    width: 100%;
+    margin-top: 80px;
+  `,
+  image: css`
+    position: relative;
+    width: 100%;
+    aspect-ratio: 1/0.31;
+  `,
+
+  textContainer: css`
+    width: 100%;
+    max-width: 1200px;
+    position: absolute;
+    bottom: 12%;
+    left: 50%;
+    transform: translateX(-50%);
+    display: flex;
+    flex-direction: column;
+    padding: 0 24px;
+    z-index: 2;
+  `,
+
+  opacity: css`
+    width: 100%;
+    aspect-ratio: 1/0.2;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    background: linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0) 100%);
+    transform: rotate(180deg);
+    z-index: 1;
+  `,
+};
