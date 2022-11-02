0d1efff00510663c3bd9f8cd7a481d80cfd149b2
schatzen <56854068+schatzen@users.noreply.github.com>
2022-11-02 19:01:46 +0900

    feat: detail 페이지 레이아웃 구현

--- src/domains/Detail/view/DetailView/DetailView.tsx
+++ src/domains/Detail/view/DetailView/DetailView.tsx
@@ -1,3 +1,17 @@
+import { Header } from "@/common/components/Header";
+import { Banner } from "../components/Banner";
+import { CardSection } from "../components/CardSection";
+import { MainCardSection } from "../components/MainCardSection";
+
 export const DetailView = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <main>
+        <Banner />
+        <MainCardSection />
+        <CardSection />
+      </main>
+    </div>
+  );
 };
