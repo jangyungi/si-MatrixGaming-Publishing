65a2ac76913e8b58c3666ec7dd608d0af736ab45
schatzen <56854068+schatzen@users.noreply.github.com>
2022-11-03 18:24:18 +0900

    feat: query params에 따른 데이터 넘기기

--- src/domains/Detail/view/DetailView/DetailView.tsx
+++ src/domains/Detail/view/DetailView/DetailView.tsx
@@ -2,15 +2,17 @@ import { Header } from "@/common/components/Header";
 import { Banner } from "../components/Banner";
 import { CardSection } from "../components/CardSection";
 import { MainCardSection } from "../components/MainCardSection";
+import { useDetailView } from "./useDetailView";
 
 export const DetailView = () => {
+  const { result } = useDetailView();
   return (
     <div>
       <Header />
       <main>
-        <Banner />
-        <MainCardSection />
-        <CardSection />
+        <Banner props={result.banner} />
+        <MainCardSection props={result.main} />
+        <CardSection props={result.contents} />
       </main>
     </div>
   );
---
+++ src/domains/Detail/view/DetailView/useDetailView.ts
@@ -0,0 +1,38 @@
+import { useRouter } from "next/router";
+import {
+  ToyItems,
+  SoulHunterItems,
+  RaphaelItems,
+  BunkerItems,
+} from "@/domains/Detail/common/models";
+import { CardType } from "../../common/models/type/card.type";
+
+export const useDetailView = () => {
+  const router = useRouter();
+  const gameName = router.query.game + "";
+
+  const getDatabyGaneName = (v: string): CardType => {
+    switch (v) {
+      case "toyNight": {
+        return ToyItems;
+      }
+      case "soulHunter": {
+        return SoulHunterItems;
+      }
+      case "raphael": {
+        return RaphaelItems;
+      }
+      case "bunker": {
+        return BunkerItems;
+      }
+
+      default: {
+        return ToyItems;
+      }
+    }
+  };
+
+  const result = getDatabyGaneName(gameName);
+
+  return { result };
+};
