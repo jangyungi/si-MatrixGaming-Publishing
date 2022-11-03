eb65191b5ad8c9bb7673193a9e2b6b7b95a780fe
schatzen <56854068+schatzen@users.noreply.github.com>
2022-11-03 18:23:27 +0900

    reafactor: 데이터 타입 분류

---
+++ src/domains/Detail/common/models/type/card.type.ts
@@ -0,0 +1,22 @@
+export type CardType = {
+  banner: BannerType;
+  main: MainCardType;
+  contents: ContentsType;
+};
+
+export type BannerType = {
+  src: string;
+  title: string;
+};
+
+export type MainCardType = {
+  src: any;
+  desc: string;
+  info: { developer: string; genre: string; status: string };
+};
+
+export type ContentsType = {
+  src: any;
+  title: string;
+  desc: string;
+}[];
