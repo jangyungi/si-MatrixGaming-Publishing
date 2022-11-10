734a3ad0f280e0874cb21936f24b8915fa1ca7be
schatzen <gomieti@naver.com>
2022-11-10 13:56:33 +0900

    update: scroll 아이콘에 animation 추가

--- src/domains/Detail/view/components/ScrollIcon/ScrollIcon.tsx
+++ src/domains/Detail/view/components/ScrollIcon/ScrollIcon.tsx
@@ -1,21 +1,41 @@
 import Image from "next/image";
-import ScrollIconImage from "@/assets/detail/icon/ic-scroll-down.svg";
-import { css } from "@emotion/react";
+import ArrowIconImage from "@/assets/detail/icon/ic-arrow.png";
+import { css, keyframes } from "@emotion/react";
 import { useScrollIcon } from "./useScrollIcon";
 import { MediaQueries } from "@/common/themes/Limit";
+import { Typography } from "@mui/material";
 
 export const ScrollIcon = () => {
   const { isScrollEnded } = useScrollIcon();
+
   return (
     <div css={st.root}>
       {!isScrollEnded ? (
-        <div css={st.image}>
-          <Image fill src={ScrollIconImage} alt={"scroll-down"} />
+        <div css={st.scrollDown}>
+          <Typography
+            fontSize="12px"
+            lineHeight={"14px"}
+            color={"#FFFFFF"}
+            textAlign="center"
+          >
+            SCROLL
+            <br />
+            DOWN
+          </Typography>
+          <div css={st.arrowImage}>
+            <Image fill src={ArrowIconImage} alt={"scroll-down"} />
+          </div>
         </div>
       ) : null}
     </div>
   );
 };
+
+const bounce = keyframes`
+0%, 20%, 50%, 80%, 100% {transform: translateY(0);}
+40% {transform: translateY(-10px);}
+60% {transform: translateY(-5px);}
+`;
 
 const st = {
   root: css`
@@ -29,14 +49,34 @@ const st = {
       bottom: 12px;
     }
   `,
-  image: css`
+  image: css``,
+
+  scrollDown: css`
     position: relative;
     width: 100px;
     height: 100px;
+    background: #1d1d1f;
+    border: 1px solid #eeeeee;
+    border-radius: 80px;
+    text-align: center;
+    margin: 0 auto;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+
+    padding: 20px 20px 14px 20px;
 
     @media ${MediaQueries.sm} {
       width: 80px;
       height: 80px;
     }
+  `,
+  arrowImage: css`
+    position: relative;
+    width: 25px;
+    height: 17px;
+    animation: ${bounce} 2s ease infinite;
   `,
 };
