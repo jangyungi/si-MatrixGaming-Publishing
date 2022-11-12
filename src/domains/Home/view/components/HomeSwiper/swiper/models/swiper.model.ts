ae6f4ba9b47b3e1db2e3ef59580fb6f9fdcc0d28
schatzen <gomieti@naver.com>
2022-11-12 15:48:33 +0900

    update: 메인 배너 이미지 변경

--- src/domains/Home/view/components/HomeSwiper/swiper/models/swiper.model.ts
+++ src/domains/Home/view/components/HomeSwiper/swiper/models/swiper.model.ts
@@ -3,6 +3,7 @@ import SwiperItem2 from "@/assets/home/swiper/banner-soulhunter.png";
 import SwiperItem3 from "@/assets/home/swiper/banner-raphael.png";
 import SwiperItem4 from "@/assets/home/swiper/banner-bunker.png";
 import SwiperItem5 from "@/assets/home/swiper/banner-projectR2.png";
+import SwiperItem6 from "@/assets/home/swiper/banner-avery.png";
 
 export const SwiperModels = [
   { src: SwiperItem1, href: "/detail/toyNight" },
@@ -10,4 +11,5 @@ export const SwiperModels = [
   { src: SwiperItem3, href: "/detail/raphael" },
   { src: SwiperItem4, href: "/detail/bunker" },
   { src: SwiperItem5, href: "" },
+  { src: SwiperItem6, href: "" },
 ];
