259608316bca3643aed6024007abe2de2c5d88a7
schatzen <gomieti@naver.com>
2022-11-12 16:08:49 +0900

    update: banner 이미지 변경

--- src/domains/Home/view/components/HomeSwiper/swiper/models/swiper.model.ts
+++ src/domains/Home/view/components/HomeSwiper/swiper/models/swiper.model.ts
@@ -2,7 +2,7 @@ import SwiperItem1 from "@/assets/home/swiper/banner-toy.png";
 import SwiperItem2 from "@/assets/home/swiper/banner-soulhunter.png";
 import SwiperItem3 from "@/assets/home/swiper/banner-raphael.png";
 import SwiperItem4 from "@/assets/home/swiper/banner-bunker.png";
-import SwiperItem5 from "@/assets/home/swiper/banner-projectR2.png";
+import SwiperItem5 from "@/assets/home/swiper/banner-projectR2.jpg";
 import SwiperItem6 from "@/assets/home/swiper/banner-avery.png";
 
 export const SwiperModels = [
