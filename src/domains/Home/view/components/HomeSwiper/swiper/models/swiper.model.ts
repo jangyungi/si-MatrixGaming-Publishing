12aa2a09a49b181205fcd120e9a0afd799597bbb
gaheeyoon <a5312559@gmail.com>
2024-02-07 00:39:28 +0900

    update: HomeSwiper, OnbeardedGames 수정
    - dtoyKnight 제거
    - NanoSpace 추가

--- src/domains/Home/view/components/HomeSwiper/swiper/models/swiper.model.ts
+++ src/domains/Home/view/components/HomeSwiper/swiper/models/swiper.model.ts
@@ -1,15 +1,15 @@
-import SwiperItem1 from "@/assets/home/swiper/banner-toy.png";
 import SwiperItem2 from "@/assets/home/swiper/banner-soulhunter.png";
 import SwiperItem3 from "@/assets/home/swiper/banner-raphael.png";
 import SwiperItem4 from "@/assets/home/swiper/banner-bunker.png";
 import SwiperItem5 from "@/assets/home/swiper/banner-projectR2.jpg";
 import SwiperItem6 from "@/assets/home/swiper/banner-avery.png";
+import SwiperItem7 from "@/assets/home/swiper/banner-nanospace.png";
 
 export const SwiperModels = [
-  { src: SwiperItem1, href: "/detail/toyNight" },
   { src: SwiperItem2, href: "/detail/soulHunter" },
   { src: SwiperItem3, href: "/detail/raphael" },
   { src: SwiperItem4, href: "/detail/bunker" },
   { src: SwiperItem5, href: "" },
   { src: SwiperItem6, href: "" },
+  { src: SwiperItem7, href: "" },
 ];
