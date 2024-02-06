2f65bce3eda2c64e6000a51e728e729fa93a0c1e
gaheeyoon <a5312559@gmail.com>
2024-02-07 00:38:23 +0900

    add: ContactUs 추가

---
+++ src/domains/Home/view/components/ContactUs/ContactUs.tsx
@@ -0,0 +1,44 @@
+import { MediaQueries } from "@/common/themes/Limit";
+import { useCustomMediaQuery } from "@/common/themes/UseCustomMediaQuery";
+import { TitleSection } from "@/domains/Home/common/components/TitleSection";
+import { css } from "@emotion/react";
+
+export const ContactUs = () => {
+  const { isMedium } = useCustomMediaQuery();
+  return (
+    <div css={st.root(isMedium)} id="contact-us">
+      <TitleSection title="Contact Us" onVisible={false} />
+      <div css={st.container}>
+        <a href="mailto:xxxxx@xxxx.xxx">xxxxx@xxxx.xxx</a>
+      </div>
+    </div>
+  );
+};
+
+const st = {
+  root: (isMedium: boolean) => css`
+    width: 100%;
+    max-width: 1200px;
+    padding: ${isMedium ? "0 16px" : "0 24px"};
+    margin: 0 auto;
+    margin-bottom: 140px;
+    padding-top: 100px;
+    padding-bottom: 200px;
+
+    @media ${MediaQueries.md} {
+      padding-top: 112px;
+      margin-bottom: 80px;
+    }
+
+    @media ${MediaQueries.sm} {
+      padding-top: 92px;
+      margin-bottom: 68px;
+    }
+  `,
+  container: css`
+    width: 100%;
+    color: #eeeeee;
+    font-size: 1.3em;
+    line-height: 1.5em;
+  `,
+};
