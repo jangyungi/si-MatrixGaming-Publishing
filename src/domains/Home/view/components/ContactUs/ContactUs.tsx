74cb8c8c5e5a3c7c2f239d40606567eda042cad0
leeleejaehong <woqhr63@gmail.com>
2024-03-03 20:27:17 +0900

    테스트

--- src/domains/Home/view/components/ContactUs/ContactUs.tsx
+++ src/domains/Home/view/components/ContactUs/ContactUs.tsx
@@ -9,7 +9,7 @@ export const ContactUs = () => {
     <div css={st.root(isMedium)} id="contact-us">
       <TitleSection title="Contact Us" onVisible={false} />
       <div css={st.container}>
-        <a href="mailto:contact@galileosky.net">contact@galileosky.net</a>
+        <a href="mailto:contact@galileosky.net">contact1@galileosky.net</a>
         <a href="https://www.galileosky.net/link.html" target="_blank">link1</a>
       </div>
     </div>
